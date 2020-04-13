0bad13ee57e16840da11b249022c18d7361d2d2f
chrisbrinkmann <brinkmcd@gmail.com>
2020-04-13 15:11:57 -0400

    add RequestSpecication to set request headers on test REST call

--- RestAssuredFundamentals/src/test/java/com/brinkmcd/restassured/tests/TestConfig.java
+++ RestAssuredFundamentals/src/test/java/com/brinkmcd/restassured/tests/TestConfig.java
@@ -3,14 +3,23 @@ package com.brinkmcd.restassured.tests;
 import org.junit.BeforeClass;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 
 public class TestConfig {
 	@BeforeClass
 	public static void setup() {
-		RestAssured.proxy("localhost", 8888);
+		// RestAssured.proxy("localhost", 8888);
 		
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.basePath = "/app";
 		RestAssured.port = 8080;
+		
+		RequestSpecification requestSpecification = new RequestSpecBuilder()
+				.addHeader("Content-Type", "application/json")
+				.addHeader("Accept", "application/json")
+				.build();
+		
+		RestAssured.requestSpecification = requestSpecification;
 	}
 }
