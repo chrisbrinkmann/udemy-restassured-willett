89abcabd06c7ed432c28293d251bf037e208aa37
chrisbrinkmann <brinkmcd@gmail.com>
2020-04-13 14:37:53 -0400

    add proxy to rest assured test

--- RestAssuredFundamentals/src/test/java/com/brinkmcd/restassured/tests/TestConfig.java
+++ RestAssuredFundamentals/src/test/java/com/brinkmcd/restassured/tests/TestConfig.java
@@ -7,6 +7,8 @@ import io.restassured.RestAssured;
 public class TestConfig {
 	@BeforeClass
 	public static void setup() {
+		RestAssured.proxy("localhost", 8888);
+		
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.basePath = "/app";
 		RestAssured.port = 8080;
